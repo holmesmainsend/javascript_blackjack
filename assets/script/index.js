5d0953fcc87c6251ba1e8fc98e34b9bb46ab8c5b
Mona Lisa <holmesmainsend@gmail.com>
2022-11-05 13:06:08 -0400

    added win/loss variables

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,7 @@
 let age = 22
 
 if(age < 21) {
-    console.log("You are not permitted to enter the gambling floor.")
+    console.log("You are not permitted on the gambling floor.")
 } else {
     console.log("Come right on in!")
 }
@@ -9,11 +9,15 @@ if(age < 21) {
 let firstCard = Math.floor(Math.random() * 10) + 2
 let secondCard = Math.floor(Math.random() * 10) + 2
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
  
 if(sum < 21) {
     console.log("Would you like to draw a new card?")
 } else if (sum === 21) {
     console.log("Blackjack!")
+    hasBlackJack = true
 } else {
     console.log("You lose.")
+    isAlive = false
 }
