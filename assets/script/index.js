3922b1e6b3c245724947d9ded959c1c5408d75b9
Mona Lisa <holmesmainsend@gmail.com>
2022-11-05 12:47:05 -0400

    added some initial if else statements

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,11 @@
 let firstCard = Math.floor(Math.random() * 10) + 2
 let secondCard = Math.floor(Math.random() * 10) + 2
 let sum = firstCard + secondCard
+ 
+if(sum < 21) {
+    console.log("Would you like to draw a new card?")
+} else if (sum === 21) {
+    console.log("Blackjack!")
+} else {
+    console.log("You lose.")
+}
